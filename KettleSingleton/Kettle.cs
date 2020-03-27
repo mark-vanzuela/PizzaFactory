e958d1536ac50968252c696951e9b98588ebf7a0
mark vanzuela <mark.vanzuela@devpartners.co>
2020-03-28 00:01:26 +0800

    include singleton projet

---
+++ KettleSingleton/Kettle.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KettleSingleton
+{
+    public sealed class Kettle
+    {
+        Kettle()
+        {
+        }
+        private static readonly object padlock = new object();
+        private static Kettle instance = null;
+        public static Kettle Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Kettle();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public State State { get; set; } = State.Empty;
+
+        public void Fill()
+        {
+            if (State == State.Empty)
+            {
+                Console.WriteLine("Filling...");
+                State = State.Boiling;
+            }
+        }
+
+        public void Drain()
+        {
+            if (State != State.Boiling)
+                Console.WriteLine("Draining...");
+            State = State.Empty;
+        }
+
+        public void Boil()
+        {
+            if (State == State.Boiling)
+                Console.WriteLine("Boiling...");
+            State = State.Boiled;
+        }
+    }
+
+    public enum State
+    {
+        Empty,
+        InProgress,
+        Boiled,
+        Boiling
+    }
+}
---
+++ KettleSingleton/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace KettleSingleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Kettle Singleton!");
+
+            Console.WriteLine("Starting...");
+
+            Kettle.Instance.Fill();
+
+            Kettle.Instance.Boil();
+
+            Kettle.Instance.Drain();
+
+            Console.ReadLine();
+        }
+    }
+}
